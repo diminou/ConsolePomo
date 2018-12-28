50875940ef6e1bb6be8e2f695f7636ea0ef9ce43
IVANOV Dmitry <dmitry.ivanov@loreal.com>
2018-12-28 16:14:55 +0100

    Formatting

--- src/Pomodoro/Library.fs
+++ src/Pomodoro/Library.fs
@@ -1,11 +1,8 @@
 namespace Pomodoro
 
 module Say =
-    let nothing name =
-        name |> ignore
-
-    let hello name =
-        sprintf "Hello %s" name
+    let nothing name = name |> ignore
+    let hello name = sprintf "Hello %s" name
 
 module Time =
     let aSecond = 1000
@@ -16,48 +13,46 @@ module Time =
     let sequenceLength = 5
 
 module Delay =
-
-    let waitInterval (i: int): unit = System.Threading.Thread.Sleep i
-    let waitAndPrint (i: int): unit =
+    let waitInterval (i : int) : unit = System.Threading.Thread.Sleep i
+    
+    let waitAndPrint (i : int) : unit =
         waitInterval i
         printfn "An interval of %i ms has ended" i
 
 module Sounds =
     open FSharpPlus
     open System
-
-    let playAlert () = Console.Beep ()
-    let playSequence (n: int) =
-        [1 .. n]
+    
+    let playAlert() = Console.Beep()
+    
+    let playSequence (n : int) =
+        [ 1..n ]
         |> List.map (fun _ -> playAlert)
         |> List.intersperse (fun _ -> Delay.waitInterval Time.interSound)
-        |> List.iter (fun f -> f ())
-        
+        |> List.iter (fun f -> f())
+
 module Glue =
     open Delay
     open Sounds
     open System
     open System.Threading
     
-    let rec pomo () =
-      waitAndPrint Time.interval25
-      playSequence Time.sequenceLength
-      waitAndPrint Time.interval5
-      playSequence Time.sequenceLength
-      pomo ()
-
-    let pomoLoop = async {
-        pomo ()
-    }
-
-
-    let listenPomo () =
-      let cancellationSource = new CancellationTokenSource()
-      // start the task, but this time pass in a cancellation token
-      Async.Start (pomoLoop, cancellationSource.Token)
-      printfn "Pomodoro timer started"
-      // wait a bit
-      match Console.Read () with
-      | _ ->
-          cancellationSource.Cancel()
-          exit 0
+    let rec pomo() =
+        waitAndPrint Time.interval25
+        playSequence Time.sequenceLength
+        waitAndPrint Time.interval5
+        playSequence Time.sequenceLength
+        pomo()
+    
+    let pomoLoop = async { pomo() }
+    
+    let listenPomo() =
+        let cancellationSource = new CancellationTokenSource()
+        // start the task, but this time pass in a cancellation token
+        Async.Start(pomoLoop, cancellationSource.Token)
+        printfn "Pomodoro timer started"
+        // wait a bit
+        match Console.Read() with
+        | _ -> 
+            cancellationSource.Cancel()
+            exit 0
